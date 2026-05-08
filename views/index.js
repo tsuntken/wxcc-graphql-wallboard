a1ee4fda803d3387b2a709c416198552152864b3
tsuntken <92815142+tsuntken@users.noreply.github.com>
2026-05-08 15:00:17 -0500

    Update index.js

--- views/index.js
+++ views/index.js
@@ -1,6 +1,6 @@
-console.log("-- START index.js");
-import { callCountByEntryPoint } from "./wallboard/callCountByEntryPoint.js";
+console.log("-- START view/wallboard/index.js");
+//import { callCountByEntryPoint } from "./wallboard/callCountByEntryPoint.js";
 //import { callStatsByAgent } from "./wallboard/callStatsByAgent.js";
 //import { callStatsByQueue } from "./wallboard/callStatsByQueue.js";
 //import { totalAgentSessionsRealTime } from "./wallboard/totalAgentSessionsRealTime.js";
-console.log("-- END index.js");
+console.log("-- END view/wallboard/index.js");
