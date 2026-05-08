893062b27ca94b2eebaab5e1eb923327765ebe13
tsuntken <92815142+tsuntken@users.noreply.github.com>
2026-05-08 14:05:55 -0500

    Update callCountByEntryPoint.js

--- controller/wallboard/callCountByEntryPoint.js
+++ controller/wallboard/callCountByEntryPoint.js
@@ -14,7 +14,7 @@ export async function callCountByEntryPoint() {
 				#TOTAL CALLS BY Entry Point
 		
 				task(
-					from: ${Date.now() - (days * 24 * 60 * 60 * 1000)} #This can be set to Date.now() - (days * 24 * 60 * 60 * 1000) for look back in days
+					from: ${Date.now() - (7 * 24 * 60 * 60 * 1000)} #This can be set to Date.now() - (days * 24 * 60 * 60 * 1000) for look back in days
 					to: ${Date.now()} #This can be set to Date.now() in ms
 					timeComparator: createdTime
 					filter: {
